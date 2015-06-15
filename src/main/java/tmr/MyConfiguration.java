fb6b8ee9a5cc90f81e92c9dab73ec98d580925d1
Mindaugas Vosylius <mindaugas.vosylius@gmail.com>
2015-06-15 12:50:41 +0300

    spring integration test

---
+++ src/main/java/tmr/MyConfiguration.java
@@ -0,0 +1,15 @@
+package tmr;
+
+import com.vaadin.spring.annotation.EnableVaadin;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author Mindaugas Vosylius
+ */
+@Configuration
+@EnableVaadin    // this imports VaadinConfiguration
+public class MyConfiguration {
+    // application specific configuration - register myBean in the context
+    
+}
--- src/main/java/tmr/TMRUI.java
+++ src/main/java/tmr/TMRUI.java
@@ -1,44 +1,73 @@
 package tmr;
 
-import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.navigator.SpringViewProvider;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+import javax.servlet.annotation.WebServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  */
 @Theme("tmr")
 @Widgetset("tmr.TMRWidgetset")
+@SpringUI
 public class TMRUI extends UI {
+    
+    // We can use either constructor autowiring or field autowiring
+    @Autowired
+    private SpringViewProvider viewProvider;
 
     @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
+    protected void init(VaadinRequest request) {
+        final VerticalLayout root = new VerticalLayout();
+        root.setSizeFull();
+        root.setMargin(true);
+        root.setSpacing(true);
+        setContent(root);
 
-        Button button = new Button("Click Me");
+        final CssLayout navigationBar = new CssLayout();
+        navigationBar.addComponent(createNavigationButton("UI Scoped View",
+                UIScopedView.VIEW_NAME));
+        navigationBar.addComponent(createNavigationButton("View Scoped View",
+                ViewScopedView.VIEW_NAME));
+        root.addComponent(navigationBar);
+
+        final Panel viewContainer = new Panel();
+        viewContainer.setSizeFull();
+        root.addComponent(viewContainer);
+        root.setExpandRatio(viewContainer, 1.0f);
+
+        Navigator navigator = new Navigator(this, viewContainer);
+        navigator.addProvider(viewProvider);
+    }
+
+    private Button createNavigationButton(String caption, final String viewName) {
+        Button button = new Button(caption);
+        button.addStyleName(ValoTheme.BUTTON_SMALL);
+        // If you didn't choose Java 8 when creating the project, convert this
+        // to an anonymous listener class
         button.addClickListener(new Button.ClickListener() {
+
             @Override
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
+            public void buttonClick(Button.ClickEvent event) {
+                getUI().getNavigator().navigateTo(
+                viewName);
             }
         });
-        layout.addComponent(button);
-
+        return button;
     }
-
-    @WebServlet(urlPatterns = "/*", name = "TMRUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = TMRUI.class, productionMode = false)
-    public static class TMRUIServlet extends VaadinServlet {
-    }
+    
 }
